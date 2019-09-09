d21a407eaa33057c9b97255ea98c0d7f0b634c23
Matheus Penchel <matheus.penchel@gmail.com>
2019-09-08 21:06:42 -0300

    Played a little bit with MVC and routing.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,23 +1,50 @@
 class CocktailsController < ApplicationController
+
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktails = Cocktail.find(params[:id])
   end
 
   def new
-    @cocktails = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def create
-    @cocktails = Cocktail.new(params[:cocktail])
-    @cocktail.save
-
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
   end
 
-  def delete
+  def edit
+  end
 
+  def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @cocktail.destroy
+
+    redirect_to cocktails_path
+  end
+
+  private
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
   get 'cocktails', to: 'cocktails#index'
-  get 'cocktails/:id', to: 'cocktails#show', as: :cocktails
-  get 'cocktails', to: 'cocktails#new'
   post 'cocktails', to: 'cocktails#create'
+
+  get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  get 'cocktails/:id/edit', to: 'cocktails#edit', as: :edit_cocktail
+  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+
+  patch 'cocktails/:id', to: 'cocktails#update'
+  delete 'cocktails/:id', to: 'cocktails#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
